9f0aa56b13d46688a29a2e60dc461b30ec5de0df
Feng <tao.feng@truvenhealth.com>
2016-09-27 18:07:10 -0400

    second commit check in

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -2,7 +2,8 @@ public class HelloWorld{
 
     public static void main(String args[]) {
         System.out.println("test ready");
-        System.out.println("another build"); 
+        System.out.println("another build");
+        System.out.println("another build 6");
     }
 
 }
